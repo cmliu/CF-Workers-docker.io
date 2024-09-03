e63ed277b5843e6e7f425ad2aa5aa1da1e51eb22
CMLiussss <24787744+cmliu@users.noreply.github.com>
2024-09-03 11:45:07 +0800

    更新 _worker.js
    修改路径匹配bug

--- _worker.js
+++ _worker.js
@@ -233,7 +233,7 @@ export default {
 		const conditions = [
 			isUuid,
 			pathname.includes('/_'),
-			pathname.includes('/r'),
+			pathname.includes('/r/'),
 			pathname.includes('/v2/user'),
 			pathname.includes('/v2/orgs'),
 			pathname.includes('/v2/_catalog'),
