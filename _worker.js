c3c584688feeb396e08daebf7d91f6831a38dff4
skillixx <131243335+skillixx@users.noreply.github.com>
2024-09-26 18:06:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ let hub_host = 'registry-1.docker.io';
 // Docker认证服务器地址
 const auth_url = 'https://auth.docker.io';
 // 自定义的工作服务器地址
-let workers_url = 'https://xxx/';
+let workers_url = 'https://hub.skillixx.com/';
 
 let 屏蔽爬虫UA = ['netcraft'];
 
