6a9451b6f58d81ae806ad4c47afa5bcf5d53aec1
QINGHLS <91734346+QINGHLS@users.noreply.github.com>
2024-12-15 19:50:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ let hub_host = 'registry-1.docker.io';
 // Docker认证服务器地址
 const auth_url = 'https://auth.docker.io';
 // 自定义的工作服务器地址
-let workers_url = 'https://xxx/';
+let workers_url = 'https://docker.netchen.us.kg';
 
 let 屏蔽爬虫UA = ['netcraft'];
 
